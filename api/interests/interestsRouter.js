185fda83663ecb529788ca9ea8c2886afb67928c
May Ng <mngmay@gmail.com>
2019-10-17 10:25:39 -0700

    Added more interest endpoints and created seed data directory

--- api/interests/interestsRouter.js
+++ api/interests/interestsRouter.js
@@ -13,4 +13,52 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  Interests.getInterestById(id)
+    .then(interest => {
+      res.status(200).json(interest);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
+// user interests
+
+router.get("/:userid", (req, res) => {
+  Interests.getUserInterests(userid)
+    .then(userInterests => {
+      res.status(200).json(userInterests);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
+router.post("/:userid", (req, res) => {
+  const userInterest = req.body;
+  Interests.addUserInterest(userInterest)
+    .then(newInterest => {
+      res.status(201).json(newInterest);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
+router.delete("/:userid/:interestid", (req, res) => {
+  const userId = req.params.userid;
+  const interestId = req.params.interestid;
+  Interests.deleteUserInterest(userId, interestId).then(res => {
+    res
+      .status(200)
+      .json({
+        message: `Successfully removed interest #${interestId} from user #${userId}`
+      })
+      .catch(err => {
+        res.status(500).json({ error: err });
+      });
+  });
+});
+
 module.exports = router;
