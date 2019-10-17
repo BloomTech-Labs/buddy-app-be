2e86ce6067f280223dc1ba2696fa96605d7e41fc
May Ng <mngmay@gmail.com>
2019-10-17 09:34:46 -0700

    created interests router and help functions

---
+++ api/interests/interests.js
@@ -0,0 +1,37 @@
+const db = require("../../data/dbconfig");
+
+module.exports = {
+  getInterests,
+  getInterestById,
+  getUserInterests,
+  addUserInterest,
+  deleteUserInterest
+};
+
+function getInterests() {
+  return db("interests");
+}
+
+function getInterestById(id) {
+  return db("interests")
+    .where({ id })
+    .first();
+}
+
+// user interests
+
+function getUserInterests() {
+  return db("user_interests").then(user_interests => user_interests);
+}
+function addUserInterest(userInterest) {
+  return db("user_interests")
+    .insert(userExperience, "id")
+    .then(result => result);
+}
+
+function deleteUserInterest(user_id, interest_id) {
+  return db("user_interests")
+    .del()
+    .where({ user_id: user_id, interests_id: interest_id })
+    .then(result => result);
+}
--- api/interests/interestsRouter.js
+++ api/interests/interestsRouter.js
@@ -1,2 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+
+const Interests = require("./interests.js");
+
+router.get("/", (req, res) => {
+  Interests.getInterests()
+    .then(interests => {
+      res.status(200).json(interests);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
+module.exports = router;
