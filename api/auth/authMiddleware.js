31639f851363189ead98d6e31f1179b70a27abfc
May Ng <mngmay@gmail.com>
2019-10-21 11:08:12 -0700

    generate token for logged in users.  compare password hash for auth.

--- api/auth/authMiddleware.js
+++ api/auth/authMiddleware.js
@@ -1,42 +1,64 @@
-const Auth = require('./auth');
-const bcrypt = require('bcryptjs');
+const Auth = require("./auth");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const secrets = require("../config/secrets.js");
 
 module.exports = {
-    validateUser,
-    validateNewUser
-
-}
+  validateUser,
+  validateNewUser,
+  jwtauth
+};
 
 function validateUser(req, res, next) {
-    const user = req.body;
-    Auth.getUserByEmail(user.email)
-    .then(returnedUser => {
-        if(returnedUser) {
-            console.log(user)
-           if(bcrypt.compareSync(user.password, returnedUser.password)) {
-               next()
-           } else{
-               res.status(401).json({message: 'Invalid Credentials'})
-           }
-        } else {
-            res.status(400).json({message: 'User not found'})
-        }
-    })
-
+  const user = req.body;
+  Auth.getUserByEmail(user.email).then(returnedUser => {
+    if (returnedUser) {
+      console.log(user);
+      if (bcrypt.compareSync(user.password, returnedUser.password)) {
+        next();
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    } else {
+      res.status(400).json({ message: "User not found" });
+    }
+  });
 }
 
 function validateNewUser(req, res, next) {
-    const user = req.body;
-    if(!user.first_name) {
-        res.status(400).json({message: 'Please provide a first name'})
-    } else if(!user.password) {
-        res.status(400).json({message: 'Please provide a password'})
-    } else if(!user.email) {
-        res.status(400).json({message: 'Please provide an email'})
-    } else if(!user.location) {
-        res.status(400).json({message: 'Please provide a zip code'})
-    } else if(!user.last_name){
-    } else {
+  const user = req.body;
+  if (!user.first_name) {
+    res.status(400).json({ message: "Please provide a first name" });
+  } else if (!user.password) {
+    res.status(400).json({ message: "Please provide a password" });
+  } else if (!user.email) {
+    res.status(400).json({ message: "Please provide an email" });
+  } else if (!user.location) {
+    res.status(400).json({ message: "Please provide a zip code" });
+  } else if (!user.last_name) {
+  } else {
+    next();
+  }
+}
+
+function jwtauth(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({
+          message: "Invalid token, please verify that you are logged in."
+        });
+      } else {
+        req.user = { email: decodedToken.email };
         next();
-    }
+      }
+    });
+  } else {
+    res.status(400).json({
+      message: "No token provided, please verify that you are logged in."
+    });
+  }
 }
