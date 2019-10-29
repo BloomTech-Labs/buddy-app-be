a00398704ddc5f8c4ebd687f86bcc232e9349461
Kevin Tou <toukevin@gmail.com>
2019-10-29 10:49:08 -0700

    Added endpoints for adding and updating activity

--- api/activities/activitiesRouter.js
+++ api/activities/activitiesRouter.js
@@ -1,2 +1,40 @@
-const express = require('express');
+const express = require("express");
+
+const Activities = require("./activities");
+
 const router = express.Router();
+
+// POST /activities
+router.post("/", (req, res) => {
+  const activity = req.body;
+
+  Activities.addActivity(activity)
+    .then(newActivity => {
+      res.status(201).json(newActivity);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error occurred while adding a new activity.",
+        error: err
+      });
+    });
+});
+
+// PUT /activities/:activityId
+router.put("/:activityId", (req, res) => {
+  const { activityId } = req.params;
+  const changes = req.body;
+
+  Activities.updateActivity(activityId, changes)
+    .then(activity => {
+      res.status(200).json(activity);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error occurred while updating an activity.",
+        error: err
+      });
+    });
+});
+
+module.exports = router;
