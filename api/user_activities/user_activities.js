ec367eec2c9a925650d4e86359288392dfee889c
Tommy Coleman <tommiilee@gmail.com>
2019-11-18 14:01:10 -0500

    added endpoint to return activities not associated with user_id

--- api/user_activities/user_activities.js
+++ api/user_activities/user_activities.js
@@ -6,7 +6,8 @@ module.exports = {
   getUserActivitiesByUserId,
   getUserActivitiesByActivityId,
   addUserActivity,
-  deleteUserActivity
+  deleteUserActivity,
+  getAllActivitiesNotAssociatedWithId
 };
 
 function getAllUserActivities() {
@@ -25,6 +26,23 @@ function getAllActivities(user_id) {
     .then(joined => {
       return db("activities as a")
         .where("organizer_id", user_id)
+        .join("users as u", "a.organizer_id", "=", "u.id")
+        .select("a.*", "u.first_name as organizer_name")
+        .then(organizer => {
+          return [...joined, ...organizer];
+        });
+    });
+}
+
+function getAllActivitiesNotAssociatedWithId(user_id) {
+  return db("user_activities as ua")
+    .join("activities as a", "ua.activity_id", "a.id")
+    .join("users as u", "a.organizer_id", "u.id")
+    .where({ user_id: !user_id })
+    .select("a.*", "u.first_name as organizer_name")
+    .then(joined => {
+      return db("activities as a")
+        .where({ organizer_id: !user_id })
         .join("users as u", "a.organizer_id", "=", "u.id")
         .select("a.*", "u.first_name as organizer_name")
         .then(organizer => {
